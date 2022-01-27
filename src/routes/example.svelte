2dde34e82ea95830c1c505306d8053d913ab0a5e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-27 11:37:05 -0600

    Added example route with multiple nested components

---
+++ src/routes/example.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from './header.svelte';
+	import Nav from './nav.svelte';
+	import Footer from './footer.svelte';
+</script>
+
+<div class="page-header">
+	<Header />
+	<Nav />
+</div>
+
+<p>This is example</p>
+<Footer />
+
+<style>
+	.page-header {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Also visit <a href="/hello">my greeting</a></p>
+<p>How about a <a href="/example">nested example?</a></p>
